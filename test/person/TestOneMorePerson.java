7812d51abda3dfa44dfd136f0375236211b59d41
gaetan <gaetan.roche@etu.univ-lyon1.fr>
2016-04-06 09:34:02 +0200

    .iml

---
+++ test/person/TestOneMorePerson.java
@@ -0,0 +1,8 @@
+/**
+ * Created by gaetan on 06/04/16.
+ */
+public class TestOneMorePerson extends TestIPerson {
+    public void initialize(){
+        //john = new OneMorePerson("Gronathan","Platteau",21);
+    }
+}
