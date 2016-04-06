6ddb02465bb2a01a0acf24d419cec45e370b9a2a
J0hn <jonathan.platteau@yahoo.fr>
2016-04-06 09:42:12 +0200

    Héhé

---
+++ test/person/TestYetAnotherPerson.java
@@ -0,0 +1,10 @@
+import people.YetAnotherPerson;
+
+/**
+ * Created by jonat on 06/04/2016.
+ */
+public class TestYetAnotherPerson extends TestIPerson {
+    public void initialize(){
+        john = new YetAnotherPerson("Gronathan", "Platteau", 1994, 11, 25);
+    }
+}
