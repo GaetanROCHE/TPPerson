4e00bb4c5c4ba075613e2746b033dcd90704338c
gaetan <gaetan.roche@etu.univ-lyon1.fr>
2016-04-06 09:41:44 +0200

    my part in adding testing classes

---
+++ test/person/TestSimplePerson.java
@@ -0,0 +1,10 @@
+import people.SimplePerson;
+
+/**
+ * Created by gaetan on 06/04/16.
+ */
+public class TestSimplePerson extends TestIPerson{
+    public void initialize(){
+        john = new SimplePerson("Gronathan","Platteau",1994,12,25);
+    }
+}
---
+++ test/person/TestSmallCodePerson.java
@@ -0,0 +1,10 @@
+import people.SmallCodePerson;
+
+/**
+ * Created by gaetan on 06/04/16.
+ */
+public class TestSmallCodePerson extends TestIPerson{
+    public void initialize(){
+        john = new SmallCodePerson("Gronathan","Platteau",1994,12,25);
+    }
+}
